3ea106afcbe8a9792449a4f2b22a0a749e57efe0
Michal Zet <michal.zadarnowski@gmail.com>
2018-04-06 01:16:39 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Test WP site for Matt',
+    subtitle: 'Fetch Data from test WP site for Matt',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        baseUrl: 'testmatt-com.stackstaging.com',
+        protocol: 'http',
+        hostingWPCOM: false,
+        useACF: false,
+        // auth: {
+        //   // If auth.user and auth.pass are filled, then the source plugin will be allowed
+        //   // to access endpoints that are protected with .htaccess.
+        //   htaccess_user: "your-htaccess-username",
+        //   htaccess_pass: "your-htaccess-password",
+        //   htaccess_sendImmediately: false,
+        // },
+        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
+        // It can help you debug specific API Endpoints problems
+        verboseOutput: true,
+        // Search and Replace Urls across WordPress content
+        searchAndReplaceContentUrls: {
+          sourceUrl: 'https://source-url.com',
+          replacementUrl: 'https://replacement-url.com',
+        },
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/sample-page">Go to sample page</Link>
+    <br />
+    <Link to="//posts/hello-world">Go to samplepost hello word</Link>
+  </div>
+)
+
+export default IndexPage
