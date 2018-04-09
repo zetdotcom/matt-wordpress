781271578c399317be1a665311fea5643c71361c
Michal Zet <michal.zadarnowski@gmail.com>
2018-04-10 00:17:39 +0100

    posts lists

---
+++ src/queries/queryAll.js
@@ -0,0 +1,29 @@
+'use strict'
+
+module.exports = `
+    {
+        allWordpressPage {
+            edges {
+                node {
+                    id
+                    slug
+                    status
+                    template
+                }
+            }
+        }
+        
+        allWordpressPost {
+            edges {
+                node {
+                    id  
+                    slug
+                    status
+                    template
+                    format
+                    excerpt
+                }
+            }
+        }
+    }
+`
