929d4bf2b76ffddd386b71079c9178da68f1a4ce
Michal Zet <michal.zadarnowski@gmail.com>
2018-04-12 01:08:41 +0100

    button component

---
+++ src/components/BlueButton/BlueButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import './BlueButton.scss'
+import '../../styles/styles.scss'
+
+const BlueButton = props => <div className="blue-button">{props.label}</div>
+
+export default BlueButton
--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -4,16 +4,25 @@ import Link from 'gatsby-link'
 import './MainHeader.scss'
 
 import Navigation from '../Navigation/Navigation'
+import BlueButton from '../../components/BlueButton/BlueButton'
+
+import logo from '../../images/logo-small.jpg'
 
 const MainHeader = () => (
   <div className="main-header">
     <Navigation />
+    <img
+      src={logo}
+      className="img-logo"
+      alt="matt wielunski personal trainer logo"
+    />
     <div className="titles container">
       <h1>Matt Wielunski</h1>
       <div className="titles-call">
         Tutaj jakies fajne motywujace zdnaie ktore bedzie musial Matt wymyslic.
         Takie zeby bylo fajnie{' '}
       </div>
+      <BlueButton label="Find out more  &rarr;" />
     </div>
   </div>
 )
